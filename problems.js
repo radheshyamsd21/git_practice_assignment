639cdc3c000f60971c4238b7242cbfc957f71f7f
AJINKYA <radheshyamsd21@gmail.com>
2022-12-01 20:15:06 +0530

    promblemsolve

---
+++ problems.js
@@ -0,0 +1,46 @@
+let num = 13;
+
+let i=1;
+
+let factor=0;
+
+for (let i=1; i<num; i++){
+
+if(i%2!=0)
+
+factor++
+}
+
+if(factor==2){
+ console.log("Yes")
+}
+else{
+console.log("No")
+}
+
+
+
+
+
+
+
+
+
+PROBLEM_2
+
+
+
+let str= ["naman"]
+
+let bag= "";
+
+ for(let i=str.length; i--){
+
+(bag+=str)
+}
+if(bag !=str){
+console.log("Yes")
+}
+else{
+ console.log("No")
+}
